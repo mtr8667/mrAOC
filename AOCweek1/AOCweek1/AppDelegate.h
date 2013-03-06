0091e6ba7ed85bf80fa50eeacf241617c5e48f21
Matthew Richter <mrichter@fullsail.edu>
2013-03-06 11:51:41 -0500

    initial commit - Hi Alexia!
    I created a new xCode (Empty) project & conpleted the practice 2 inside
    the application didFinishLaunchingWithOptions function

---
+++ AOCweek1/AOCweek1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOCweek1
+//
+//  Created by Matthew T. Richter on 3/6/13.
+//  Copyright (c) 2013 Matthew T. Richter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
