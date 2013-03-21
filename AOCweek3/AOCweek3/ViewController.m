84edab58c1c45aa937579226fba0fbda0bcde163
Matthew Richter <mrichter@fullsail.edu>
2013-03-21 19:07:09 -0400

    Making progress my displayAplertWithString is working

--- AOCweek3/AOCweek3/ViewController.m
+++ AOCweek3/AOCweek3/ViewController.m
@@ -14,6 +14,30 @@
 
 @implementation ViewController
 
+
+- (void)viewDidLoad
+{
+    self.view.backgroundColor = [UIColor colorWithRed:1 green:1 blue:0.961 alpha:1]; /*#fffff5*/
+    
+    appended = [[NSMutableString alloc]init];
+
+    string1 = [[NSString alloc]initWithString:@"Warning!"];
+    string2 = [[NSString alloc]initWithString:@"You are currently not connected to the internet!"];
+    numberToAdd1 = 4;
+    numberToAdd2 = 6;
+    numberToCompare1 = 45;
+    numberToCompare1 = 38;
+    
+    NSMutableString * appendedValue =   [self append:string1 append2:string2];
+                                        [self displayAlertWithString:appendedValue];
+    
+    NSLog(@"Appended String = %@",appendedValue);
+    
+
+    
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
 + (int)add:(NSInteger)n1 add2:(NSInteger)n2
 {
     return (n1 + n2);
@@ -30,30 +54,22 @@
 - (NSMutableString *)append:(NSString *)str1 append2:(NSString *)str2
 {
     
-    if (str1 > 0 || str2 > 0) {
+    if (str1 != nil || str2 != nil) {
         [appended appendFormat:@"%@",str1];
-        [appended appendFormat:@", %@",str2];
-     
+        [appended appendFormat:@" %@",str2];
+        
     }
     return appended;
 }
 
-- (void)viewDidLoad
+- (void)displayAlertWithString:(NSString *)alertString
 {
-    appended = [[NSMutableString alloc]init];
-
-    string1 = [[NSString alloc]initWithString:@"Warning!"];
-    string2 = [[NSString alloc]initWithString:@"you are currently not connected to the internet!"];
-    numberToAdd1 = 4;
-    numberToAdd2 = 6;
-    numberToCompare1 = 45;
-    numberToCompare1 = 38;
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert" message:alertString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    if (alertView != nil)
+    {
+        [alertView show];
+    }
     
-    NSMutableString * appendedValue = [self append:string1 append2:string2];
-    
-    NSLog(@"Appended String = %@",appendedValue);
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 
