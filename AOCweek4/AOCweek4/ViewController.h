f9d904cb6833b5a8ec1b0b497ec81bc39598dfe1
Matthew Richter <mrichter@fullsail.edu>
2013-03-27 16:40:54 -0400

    pluggin along - this is my initial commit for week4 aside from setup
    Oh and BTW - thanks for the clarification on the in the it's vs it in
    the week three discussion - I really appreciated the reference link - I
    know better .... lazy lazy lazy - good wake up call - thanks !

--- AOCweek4/AOCweek4/ViewController.h
+++ AOCweek4/AOCweek4/ViewController.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIAlertViewDelegate>
+{
+    UILabel *pleaseEnterUsernameLabel;
+    UITextField *textField;
+    NSString *userText;
+}
+
 
 @end
--- AOCweek4/AOCweek4/ViewController.m
+++ AOCweek4/AOCweek4/ViewController.m
@@ -16,8 +16,52 @@
 
 - (void)viewDidLoad
 {
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    textField = [[UITextField alloc] initWithFrame:CGRectMake(110.0f, 10.0f, 200.0f, 30.0f)];
+    if (textField != nil) {
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+        [self.view addSubview:textField];
+    }
+    
+    UILabel *textFieldLabel = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 10.0f, 100.0f, 30.0f)];
+    if (textFieldLabel != nil) {
+        textFieldLabel.text = @"Username:";
+        textFieldLabel.backgroundColor = [UIColor lightGrayColor];
+        textFieldLabel.textColor = [UIColor blackColor];
+        
+        [self.view addSubview:textFieldLabel];
+    }
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    if (button != nil) {
+        button.frame = CGRectMake (210.0f,50.0f,100.0f,30.0f);
+        [button setTitle:@"Login" forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:button];
+    }
+
+    pleaseEnterUsernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 100.0f, 320.0f, 60.0f)];
+    if (pleaseEnterUsernameLabel != nil) {
+        pleaseEnterUsernameLabel.backgroundColor =[UIColor colorWithRed:0.212 green:0.212 blue:0.212 alpha:1]; /*#363636*/
+        
+        pleaseEnterUsernameLabel.text = @"Please Enter Username";
+        pleaseEnterUsernameLabel.textAlignment = NSTextAlignmentCenter;
+        pleaseEnterUsernameLabel.textColor = [UIColor whiteColor];
+        [self.view addSubview:pleaseEnterUsernameLabel];
+
+    }
+
+
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)onClick
+{
+    userText = [textField text];
+    
+    NSLog(@"Username: %@", userText);
 }
 
 - (void)didReceiveMemoryWarning
